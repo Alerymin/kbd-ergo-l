20459ce898b1330e5977659744096d6b4d7296d9
Alerymin <rdev.e8t01@silomails.com>
2025-08-08 19:25:30 +0200

    add installer

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cat << EOF
+#######################################
+ Ergo-L layout xkb installer
+ by Alerymin
+ https://github.com/Alerymin/kbd-ergo-l
+########################################
+EOF
+
+# Root permissions check
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script needs to be run as root"
+    exit
+fi
+
+echo "Copying ergol.map.gz"
+cp ./ergol.map.gz /usr/share/kbd/keymaps/
+
+echo "Copying ergol-iso.map.gz"
+cp ./ergol-iso.map.gz /usr/share/kbd/keymaps/
+
+echo "Done"
